e8adafa287e64e7c07ea69057761016ed5ad9977
Aleksey Pechurin <pe4urin@yandex.ru>
2023-05-05 13:45:04 +0300

    start

---
+++ src/page/Home-page.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Page Home</p>
+</template>
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { createRouter,
+ createWebHashHistory} from "vue-router";
+
+const routerHistory = createWebHashHistory();
+
+import HomePage from './page/Home-page.vue';
+import AboutPage from './page/About-page.vue';
+import NotFound from './page/NotFound-page.vue';
+
+const routers = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage,
+    },
+    {
+      path: '/:CatchAll(.*)',
+      name: '404',
+      component: NotFound,
+    }
+  ]
+})
+
+export default routers;
